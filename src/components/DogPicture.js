5e2fb3ee008811d7a50f8c47c04aa770a5fb089a
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-06 09:53:11 -0500

    Add stepper

--- src/components/DogPicture.js
+++ src/components/DogPicture.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Button from 'material-ui/Button'
+import TextField from 'material-ui/TextField'
 
-const DogPicture = ({url}) => {
-  return (
-    <div style={styles.wrapper}>
-      <img src={url} />
-    </div>
-  )
+export default class DogPicture extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      imageUrl: '',
+      breed: ''
+    }
+  }
+
+  getImageUrl = () => {
+    fetch(`https://dog.ceo/api/breed/${this.state.breed}/images/random`)
+    .then(response => response.json())
+    .then(data => this.setState({ imageUrl: data.message }))
+    this.props.updateBreed(this.state.breed)
+    this.props.updateScore()
+  }
+
+  handleChange = event => {
+    this.setState({ breed: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <TextField fullWidth id="breed" label="What breed do you think you are?" onChange={this.handleChange} margin="normal" />
+        <Button onClick={this.getImageUrl} raised color="primary">
+          Show This Breed
+        </Button>
+        <div style={styles.wrapper}>
+          {this.state.breed.length > 0 ? <img src={this.state.imageUrl} /> : null}
+        </div>
+      </div>
+    )
+  }
 }
 
 const styles = {
@@ -13,5 +44,3 @@ const styles = {
     margin: 8
   }
 }
-
-export default DogPicture
--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -3,37 +3,100 @@ import Paper from 'material-ui/Paper'
 import Grid from 'material-ui/Grid'
 import DogPicture from '../components/DogPicture'
 import Button from 'material-ui/Button'
+import Radio, { RadioGroup } from 'material-ui/Radio'
+import { FormLabel, FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form'
+import Stepper, { Step, StepLabel } from 'material-ui/Stepper'
+import Typography from 'material-ui/Typography'
+import TextField from 'material-ui/TextField'
 
 export default class Survey extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      imageUrl: ''
+      name: '',
+      spirit: '',
+      score: 7,
+      breed: '',
+      activeStep: 0
     }
   }
 
-  getImageUrl = () => {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(data => this.setState({ imageUrl: data.message }))
-  }
+  updateSpirit = (event, value) => this.setState({ spirit: value, score: this.state.score - 1 })
 
-  componentDidMount() {
-    this.getImageUrl()
+  updateBreed = breed => this.setState({ breed })
+
+  updateName = name => this.setState({ name })
+
+  updateScore = () => this.setState({ score: this.state.score - 1 })
+
+  handleNext = () => this.setState({ activeStep: this.state.activeStep + 1 })
+
+  handleBack = () => this.setState({ activeStep: this.state.activeStep - 1 })
+
+  handleReset = () => this.setState({ activeStep: 0 })
+
+  getStepContent = step => {
+    switch (step) {
+      case 0:
+        return (
+          <TextField fullWidth id="name" label="What is your name?" onChange={this.updateName} margin="normal" />          
+        )
+      case 1:
+        return (
+          <FormControl component="fieldset" required>
+          <FormLabel component="legend">Are you a dog or a cat?</FormLabel>
+          <RadioGroup aria-label="spirit" name="spirit" value={this.state.spirit} onChange={this.updateSpirit}>
+            <FormControlLabel value="cat" control={<Radio />} label="Cat" />
+            <FormControlLabel value="dog" control={<Radio />} label="Dog" />
+          </RadioGroup>
+          {this.state.spirit === 'dog' ? (
+            <DogPicture updateBreed={this.updateBreed} updateScore={this.updateScore} />
+          ) : null}
+        </FormControl>
+        )
+      case 2:
+        return 'Some questions about food'
+      default:
+        return 'Thanks for submitting the survey, we will be in touch.'
+    }
   }
 
   render() {
-    console.log(this.state.imageUrl)
+    console.log(this.state.breed)
+    const steps = ['General information', 'Assign your spirit animal', 'Food Preference']
     return (
       <div style={styles.wrapper}>
         <Grid container spacing={24}>
           <Grid item xs={12}>
             <Paper style={styles.paper}>
-              <Button onClick={this.getImageUrl} raised color="primary">
-                Show Another Dog
-              </Button>
-              <DogPicture url={this.state.imageUrl} />
+              {/* <h4>{this.state.score} questions left to answer.</h4> */}
+              {this.state.activeStep === steps.length ? (
+                    <Button onClick={this.handleReset}>Reset</Button>
+                ) : (
+                  <div>
+                    <div>
+                      <Button disabled={this.state.activeStep === 0} onClick={this.handleBack}>
+                        Back
+                      </Button>
+                      <Button raised color="primary" onClick={this.handleNext}>
+                        {this.state.activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+                      </Button>
+                    </div>
+                  </div>
+                )}
+              <Stepper activeStep={this.state.activeStep}>
+                {steps.map((label) => {
+                  return (
+                    <Step key={label}>
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  )
+                })}
+              </Stepper>
+                {this.getStepContent(this.state.activeStep)}
+              <div>
+              </div>
             </Paper>
           </Grid>
         </Grid>
